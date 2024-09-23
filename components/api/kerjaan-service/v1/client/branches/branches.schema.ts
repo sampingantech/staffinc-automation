92b10cb953f4d90afadd62247485c3ca59773daa
staffinc-equipment <fauzi@staffinc.co>
2024-09-23 15:31:55 +0700

    Add initial project configuration and enhance branch and payroll group functionalities
    - Update `branchesData` class to handle optional parameters in `createBranches` method
    - Introduce `createBranchPayload` method in `branchesSchema` to validate branch creation data
    - Extend `PayrollGroupsData` class with optional parameters in `createPayrollGroup` method
    - Add `createPayrollGroupPayload` method in `PayrollGroupsSchema` for payload validation
    - Modify test functions to accept data parameters for creating branches and payroll groups
    - Update dependencies to include TypeScript and xlsx library

--- components/api/kerjaan-service/v1/client/branches/branches.schema.ts
+++ components/api/kerjaan-service/v1/client/branches/branches.schema.ts
@@ -35,4 +35,27 @@ export class branchesSchema {
         });
 
     }
+    createBranchPayload() {
+        return z.object({
+            name: z.string().optional(),
+            attendance_approval_type: z.string(),
+            shifting_configuration_type: z.string(),
+            branch_location: z.string(),
+            position: z.array(z.number()),
+            radius: z.number(),
+            enable_geofence: z.boolean(),
+            location_detail: z.string(),
+            selfie_instruction: z.string(),
+            attendance_type: z.string(),
+            require_checkout: z.boolean(),
+            effective_working_days: z.array(z.number()),
+            start_time_utc: z.string(),
+            end_time_utc: z.string(),
+            attendance_source: z.string(),
+            province_id: z.number(),
+            city_id: z.number(),
+            district_id: z.number(),
+            client_id: z.number()
+        })
+    }
 }
